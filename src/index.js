e3fb7f78e153bb9552dd048452b86aff1d67eb14
Monica <monijerobon@gmail.com>
2023-03-25 00:24:42 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault()
+    tasklisting(e.target["new-task-description"].value)
+
+  })
 });
+function tasklisting(captureText){
+  let list = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click',delFunc)
+  btn .textContent = 'X'
+  list.textContent = `${captureText} `
+  list.appendChild(btn)
+  document.querySelector("#tasks").appendChild(list)
+}
+function delFunc(e){
+  e.target.parentNode.remove()
+}
